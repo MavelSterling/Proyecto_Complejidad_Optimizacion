b27b01d604ec822fe350a61354ed6d4fce9056ec
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2023-10-07 13:58:46 -0500

    Update PlantaGUIFuentes.py

--- PlantaGUIFuentes.py
+++ PlantaGUIFuentes.py
@@ -8,8 +8,9 @@ class EnergyApp(tk.Tk):
         super().__init__()
 
         self.title("Interfaz de Energía")
-        self.geometry("600x400")
+        self.geometry("600x450")
 
+        # Campos para los datos de entrada
         ttk.Label(self, text="Número de días (n):").grid(column=0, row=0)
         self.days_var = tk.IntVar()
         ttk.Entry(self, textvariable=self.days_var).grid(column=1, row=0)
@@ -26,40 +27,45 @@ class EnergyApp(tk.Tk):
         self.capacities_var = tk.StringVar()
         ttk.Entry(self, textvariable=self.capacities_var).grid(column=1, row=3)
 
-        ttk.Label(self, text="Demanda (Formato: día1cliente1,día1cliente2,...,díaNclienteM):").grid(column=0, row=4, sticky='w')
-        self.demand_var = tk.StringVar()
-        ttk.Entry(self, textvariable=self.demand_var, width=50).grid(column=1, row=4)
-
-        ttk.Label(self, text="Pagos individuales por MW (cliente1,cliente2,...):").grid(column=0, row=5, sticky='w')
+        ttk.Label(self, text="Pagos individuales por MW (m clientes):").grid(column=0, row=4)
         self.ps_var = tk.StringVar()
-        ttk.Entry(self, textvariable=self.ps_var).grid(column=1, row=5)
+        ttk.Entry(self, textvariable=self.ps_var).grid(column=1, row=4)
 
-        ttk.Button(self, text="Ejecutar", command=self.execute_model).grid(column=1, row=6)
+        ttk.Label(self, text="Porcentaje mínimo de demanda a cubrir:").grid(column=0, row=5)
+        self.min_demand_percentage_var = tk.DoubleVar(value=0.50)
+        ttk.Entry(self, textvariable=self.min_demand_percentage_var).grid(column=1, row=5)
 
-        self.result_label = ttk.Label(self, text="Resultado: ")
-        self.result_label.grid(column=0, row=7, columnspan=2)
+        ttk.Label(self, text="Porcentaje máximo para hidroeléctrica:").grid(column=0, row=6)
+        self.hydro_max_percentage_var = tk.DoubleVar(value=0.80)
+        ttk.Entry(self, textvariable=self.hydro_max_percentage_var).grid(column=1, row=6)
+
+        ttk.Label(self, text="Días máximos consecutivos a ese porcentaje:").grid(column=0, row=7)
+        self.hydro_max_consecutive_days_var = tk.IntVar(value=1)
+        ttk.Entry(self, textvariable=self.hydro_max_consecutive_days_var).grid(column=1, row=7)
+
+        self.result_var = tk.StringVar(value="Ganancia neta:")
+        ttk.Label(self, textvariable=self.result_var).grid(column=1, row=8)
+
+        ttk.Button(self, text="Ejecutar", command=self.execute_model).grid(column=1, row=9)
 
     def execute_model(self):
-        # Captura los valores desde la interfaz y escribe el archivo Datos.dzn
+        # Genera Datos.dzn con los valores de la interfaz
         with open("Datos.dzn", "w") as file:
-            n_val = self.days_var.get()
-            m_val = self.clients_var.get()
+            file.write(f"n = {self.days_var.get()};\n")
+            file.write(f"m = {self.clients_var.get()};\n")
             costs = self.costs_var.get().split(',')
-            capacities = self.capacities_var.get().split(',')
-            demand = self.demand_var.get().split(',')
-            ps = self.ps_var.get().split(',')
-
-            # Escribiendo los valores al archivo Datos.dzn
-            file.write(f"n = {n_val};\n")
-            file.write(f"m = {m_val};\n")
             file.write(f"costs = [{costs[0]}, {costs[1]}, {costs[2]}];\n")
+            capacities = self.capacities_var.get().split(',')
             file.write(f"capacities = [{capacities[0]}, {capacities[1]}, {capacities[2]}];\n")
-            file.write(f"demand = [|{' | '.join(demand)}|];\n")  # Reestructura la demanda a un formato 2D
-            file.write(f"Ps = [{', '.join(ps)}];\n")
+            ps = ', '.join(self.ps_var.get().split(','))
+            file.write(f"Ps = [{ps}];\n")
+            file.write(f"min_demand_percentage = {self.min_demand_percentage_var.get()};\n")
+            file.write(f"hydro_max_percentage = {self.hydro_max_percentage_var.get()};\n")
+            file.write(f"hydro_max_consecutive_days = {self.hydro_max_consecutive_days_var.get()};\n")
 
-        # Ejecuta el modelo de MiniZinc y captura el resultado
-        result = os.popen("minizinc PlantaEnergia.mzn Datos.dzn").read()
-        self.result_label.config(text=f"Resultado: {result}")
+        # Ejecuta el modelo con el solver especificado
+        result = os.popen("minizinc --solver COIN-BC PlantaEnergia.mzn Datos.dzn").read()
+        self.result_var.set(result)
 
 if __name__ == "__main__":
     app = EnergyApp()
