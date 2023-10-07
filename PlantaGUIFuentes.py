11d28c2a5bf481523ee3269743f192e74f471d4c
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2023-10-07 12:52:09 -0500

    Update PlantaGUIFuentes.py

--- PlantaGUIFuentes.py
+++ PlantaGUIFuentes.py
@@ -8,7 +8,7 @@ class EnergyApp(tk.Tk):
         super().__init__()
 
         self.title("Interfaz de Energía")
-        self.geometry("500x400")
+        self.geometry("600x400")
 
         ttk.Label(self, text="Número de días (n):").grid(column=0, row=0)
         self.days_var = tk.IntVar()
@@ -26,24 +26,40 @@ class EnergyApp(tk.Tk):
         self.capacities_var = tk.StringVar()
         ttk.Entry(self, textvariable=self.capacities_var).grid(column=1, row=3)
 
-        # ... puedes agregar más campos según lo necesario
+        ttk.Label(self, text="Demanda (Formato: día1cliente1,día1cliente2,...,díaNclienteM):").grid(column=0, row=4, sticky='w')
+        self.demand_var = tk.StringVar()
+        ttk.Entry(self, textvariable=self.demand_var, width=50).grid(column=1, row=4)
 
-        ttk.Button(self, text="Ejecutar", command=self.execute_model).grid(column=1, row=10)
+        ttk.Label(self, text="Pagos individuales por MW (cliente1,cliente2,...):").grid(column=0, row=5, sticky='w')
+        self.ps_var = tk.StringVar()
+        ttk.Entry(self, textvariable=self.ps_var).grid(column=1, row=5)
+
+        ttk.Button(self, text="Ejecutar", command=self.execute_model).grid(column=1, row=6)
+
+        self.result_label = ttk.Label(self, text="Resultado: ")
+        self.result_label.grid(column=0, row=7, columnspan=2)
 
     def execute_model(self):
         # Captura los valores desde la interfaz y escribe el archivo Datos.dzn
         with open("Datos.dzn", "w") as file:
-            file.write(f"n = {self.days_var.get()};\n")
-            file.write(f"m = {self.clients_var.get()};\n")
+            n_val = self.days_var.get()
+            m_val = self.clients_var.get()
             costs = self.costs_var.get().split(',')
-            file.write(f"costs = [{costs[0]}, {costs[1]}, {costs[2]}];\n")
             capacities = self.capacities_var.get().split(',')
-            file.write(f"capacities = [{capacities[0]}, {capacities[1]}, {capacities[2]}];\n")
-            # ... más datos según sean necesarios
+            demand = self.demand_var.get().split(',')
+            ps = self.ps_var.get().split(',')
 
-        # Ejecuta el modelo de MiniZinc
-        os.system("minizinc PlantaEnergia.mzn Datos.dzn")
-        # Aquí puedes añadir código para capturar la salida de MiniZinc y mostrarla en la interfaz
+            # Escribiendo los valores al archivo Datos.dzn
+            file.write(f"n = {n_val};\n")
+            file.write(f"m = {m_val};\n")
+            file.write(f"costs = [{costs[0]}, {costs[1]}, {costs[2]}];\n")
+            file.write(f"capacities = [{capacities[0]}, {capacities[1]}, {capacities[2]}];\n")
+            file.write(f"demand = [|{' | '.join(demand)}|];\n")  # Reestructura la demanda a un formato 2D
+            file.write(f"Ps = [{', '.join(ps)}];\n")
+
+        # Ejecuta el modelo de MiniZinc y captura el resultado
+        result = os.popen("minizinc PlantaEnergia.mzn Datos.dzn").read()
+        self.result_label.config(text=f"Resultado: {result}")
 
 if __name__ == "__main__":
     app = EnergyApp()
